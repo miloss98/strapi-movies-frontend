95c388aa843da57c933c87b7c0a659294c5e15f9
stojanovic98m <stojanovic98m@gmail.com>
2022-07-29 12:25:39 +0200

    about page dnoe

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,28 @@
 import "./../styles/about.css";
 const About = () => {
-  return <div className="about-wrapper">About</div>;
+  return (
+    <div className="about-wrapper">
+      <h1 className="about-title"> About </h1>
+      <div className="about-container">
+        <p className="about">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae hic
+          voluptate perspiciatis reiciendis fugit provident maxime earum.
+          Quaerat reiciendis a eaque repellat ut odit, facilis suscipit odio,
+          soluta quisquam placeat.
+        </p>
+        <p className="about">
+          {" "}
+          Lorem ipsum dolor sit amet consectetur, adipisicing Laborum aliquam
+          ipsam sit repellendus culpa hic nobis odio quidem. Aperiam est
+          recusandae quo natus. elit. Aliquid maiores earum id magnam labore
+          eveniet, facilis excepturi officia saepe velit, quasi non doloremque.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+          asperiores est quidem recusandae voluptate explicabo. Qui minus
+          laborum quis quidem doloremque consequuntur natus alias soluta!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
