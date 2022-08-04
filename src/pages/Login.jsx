3ad11f16bc019a0ca41f0530f10baa1f6f5fd82c
stojanovic98m <stojanovic98m@gmail.com>
2022-08-04 16:32:32 +0200

    login styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,73 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { AuthContext } from "../modules/authcontext";
+import { Link } from "react-router-dom";
+import "./../styles/pages/login.css";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const { user, setUser, isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+  let navigate = useNavigate();
+
+  const login = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:1337/api/auth/local", {
+        identifier: "markomarkovic@gmaild.com",
+        password: "Strapi123",
+      })
+      .then((response) => {
+        console.log(response.data);
+        console.log("User profile", response.data.user);
+        console.log("User token", response.data.jwt);
+        setUser(response.data.user);
+        setIsLoggedIn(true);
+        navigate("/watchlist");
+      })
+      .catch((error) => {
+        setMsg("Email / password is incorrect!");
+        setIsLoggedIn(false);
+        setTimeout(() => {
+          setMsg("");
+        }, "2000");
+      });
+  };
+  return (
+    <div className="login-wrapper">
+      <section className="login-container">
+        <div className="avatar-container"></div>
+        <p> {msg} </p>
+        <form className="form">
+          <input
+            className="login-input-field"
+            type="text"
+            placeholder="Email"
+            name="email"
+          />
+          <input
+            className="login-input-field"
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+          <button className="login-btn" onClick={login}>
+            Login{" "}
+          </button>
+        </form>
+
+        <p className="register-redirect">
+          Don't have an account?{" "}
+          <Link className="link-to-register" to="/register">
+            {" "}
+            Register{" "}
+          </Link>
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
