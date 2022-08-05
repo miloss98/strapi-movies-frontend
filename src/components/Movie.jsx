6afa786b25e0b44ce9d266542574359e5259798a
stojanovic98m <stojanovic98m@gmail.com>
2022-08-05 11:28:57 +0200

    added rating on home screen

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,6 +2,9 @@ import "./../styles/components/movie.css";
 import { useContext } from "react";
 import { MoviesContext } from "./../modules/context";
 import { useNavigate } from "react-router-dom";
+//
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const url = "http://localhost:1337";
 
@@ -51,9 +54,14 @@ const Movie = () => {
               <p>
                 Duration: <span> {attributes?.duration}</span>
               </p>
-              <p>
-                Rating: <span> {attributes?.rating} </span>
-              </p>
+              <div style={{ height: "50px", width: "50px" }}>
+                <CircularProgressbar
+                  value={attributes?.rating}
+                  minValue={1}
+                  maxValue={10}
+                  text={attributes?.rating}
+                />
+              </div>
             </section>
             <section className="read-more-container">
               <button
