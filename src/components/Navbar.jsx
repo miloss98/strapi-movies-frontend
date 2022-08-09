40440ac5b6f4c7ca444f570698fb860b981d06de
stojanovic98m <stojanovic98m@gmail.com>
2022-08-09 12:00:04 +0200

    new logout icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { AuthContext } from "../modules/authcontext";
 import { Home, Profile, Login, About } from "./../pages/index";
 import "./../styles/components/navbar.css";
+import { BiLogOut } from "react-icons/bi";
+
 const Navbar = () => {
   const { user, setUser, setIsLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
@@ -41,7 +43,7 @@ const Navbar = () => {
           </NavLink>{" "}
           <div className="logout-container">
             <button onClick={logout} className="logout-btn">
-              {" "}
+              <BiLogOut className="logout-icon" />
             </button>
           </div>
         </>
