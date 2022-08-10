5e8d26f383ab1a72aec50fe272c25762141dfc18
stojanovic98m <stojanovic98m@gmail.com>
2022-08-10 12:16:58 +0200

    logo image is now link to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import Navbar from "./Navbar";
 import "./../styles/components/header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className="header">
-      <section className="image-container"></section>
+      <section
+        className="image-container"
+        onClick={() => navigate("/")}
+      ></section>
       <section className="title-container">
         <h1 className="title"> Movies App </h1>
       </section>
